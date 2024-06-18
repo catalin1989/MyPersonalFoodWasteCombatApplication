f695adb89c2a5992d52cfe69e155db8ceafb6f59
CatalinChira <catalin_chira05@yahoo.com>
2024-06-18 22:45:30 +0300

    changed to jar

--- src/main/java/MyFoodWasteCombat/FoodWasteCombat/FoodWasteCombatApplication.java
+++ src/main/java/MyFoodWasteCombat/FoodWasteCombat/FoodWasteCombatApplication.java
@@ -2,10 +2,19 @@ package MyFoodWasteCombat.FoodWasteCombat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class FoodWasteCombatApplication {
 
+public class FoodWasteCombatApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FoodWasteCombatApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FoodWasteCombatApplication.class, args);
 	}
