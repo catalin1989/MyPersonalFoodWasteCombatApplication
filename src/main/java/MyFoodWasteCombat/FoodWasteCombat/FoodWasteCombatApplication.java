02609b494f05b1ac33a80b57c8637ecd09caefe7
CatalinChira <catalin_chira05@yahoo.com>
2024-06-24 11:15:33 +0300

    testing env

--- src/main/java/MyFoodWasteCombat/FoodWasteCombat/FoodWasteCombatApplication.java
+++ src/main/java/MyFoodWasteCombat/FoodWasteCombat/FoodWasteCombatApplication.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 
@@ -23,9 +20,7 @@ public class FoodWasteCombatApplication implements CommandLineRunner {
 	@Value("${PROD_DB_PASSWORD}")
 	private String dbPassword;
 
-
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		SpringApplication.run(FoodWasteCombatApplication.class, args);
 	}
 
@@ -33,7 +28,8 @@ public class FoodWasteCombatApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("SPRING_DATASOURCE_URL: " + datasourceUrl);
 		System.out.println("PROD_DB_USERNAME: " + dbUsername);
-		System.out.println("PROD_DB_PASSWORD: " + dbPassword);  // Be careful with logging passwords!
+		System.out.println("PROD_DB_PASSWORD: " + dbPassword); // Be careful with logging passwords!
 	}
+}
 
 }
