aa20b485d001220c78aaf1fe868149a3afbe07ed
CatalinChira <catalin_chira05@yahoo.com>
2024-06-24 12:08:11 +0300

    testing env

--- src/main/java/MyFoodWasteCombat/FoodWasteCombat/FoodWasteCombatApplication.java
+++ src/main/java/MyFoodWasteCombat/FoodWasteCombat/FoodWasteCombatApplication.java
@@ -9,27 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-public class FoodWasteCombatApplication implements CommandLineRunner {
-
-	@Value("${SPRING_DATASOURCE_URL}")
-	private String datasourceUrl;
-
-	@Value("${PROD_DB_USERNAME}")
-	private String dbUsername;
-
-	@Value("${PROD_DB_PASSWORD}")
-	private String dbPassword;
+public class FoodWasteCombatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodWasteCombatApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("SPRING_DATASOURCE_URL: " + datasourceUrl);
-		System.out.println("PROD_DB_USERNAME: " + dbUsername);
-		System.out.println("PROD_DB_PASSWORD: " + dbPassword); // Be careful with logging passwords!
-	}
 }
 
 
